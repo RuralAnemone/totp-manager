d74993c26838450c5b22bab8b35a98ed49d85aef
Isaiah <64103005+RuralAnemone@users.noreply.github.com>
2024-06-29 17:32:19 -0500

    commit all them changes

---
+++ src/components/TOTPEntry.vue
@@ -0,0 +1,20 @@
+<!-- card-like, list of these in a div off to the left (or right? user preference, eventually) -->
+<script setup lang="ts">
+    defineProps<{
+        secretKey: String,
+        period: Number,
+        digits: Number,
+        algorithm: String
+    }>()
+</script>
+<template>
+    <p>
+        <slot></slot>
+    </p>
+    <ul>
+        <li>key: {{ secretKey }}</li>
+        <li>period: {{ period }}</li>
+        <li>length: {{ digits }}</li>
+        <li>algorithm: {{ algorithm }}</li>
+    </ul>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,22 @@
 <script setup lang="ts">
-import TheWelcome from "../components/TheWelcome.vue";
+import { ref } from 'vue';
+import TOTPEntry from "@/components/TOTPEntry.vue"
+
+let id = ref(0);
+const entries = ref([
+    {
+        id: id.value++,
+        secretKey: [...btoa("according to all known laws of aviation,").toUpperCase()].filter(c=>c!=="=").join``,
+        digits: 6,
+        period: 30,
+        algorithm: "SHA-1"
+    },
+
+])
 </script>
 
 <template>
     <main>
-        <TheWelcome />
+        <TOTPEntry v-for="entry in entries" :secret-key="entry.secretKey" :digits="entry.digits" :period="entry.period" :algorithm="entry.algorithm" :key="entry.id">{{ entry.id }}</TOTPEntry>
     </main>
 </template>
