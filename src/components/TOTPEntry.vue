81232545f2e4fb6c996edf3e08af92204704983e
Isaiah <64103005+RuralAnemone@users.noreply.github.com>
2024-06-29 18:02:32 -0500

    kinda works now

--- src/components/TOTPEntry.vue
+++ src/components/TOTPEntry.vue
@@ -1,15 +1,15 @@
 <!-- card-like, list of these in a div off to the left (or right? user preference, eventually) -->
-<script setup lang="ts">
+<script setup>
 import * as OTPAuth from "otpauth"
-    defineProps<{
-        issuer: string,
-        label: string
-        secretKey: string,
-        period: number,
-        digits: number,
-        algorithm: string,
-        otpObject: OTPAuth.TOTP
-    }>()
+defineProps({
+    issuer: String,
+    label: String,
+    secretKey: String,
+    period: Number,
+    digits: Number,
+    algorithm: String,
+    otpObject: OTPAuth.TOTP
+})
 </script>
 <template>
     <p>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,4 @@
-<script setup lang="ts">
+<script setup>
 import { ref } from 'vue';
 import TOTPEntry from "@/components/TOTPEntry.vue"
 import * as OTPAuth from "otpauth";
@@ -9,27 +9,40 @@ const entries = ref([
         id: id.value++,
         issuer: "ACME",
         label: "test totp config #1",
-        secretKey: [...btoa("according to all known laws of aviation,").toUpperCase()].filter(c=>c!=="=").join(""),
+        secretKey: "JBSWY3DPEHPK3PXP",
         digits: 6,
         period: 30,
         algorithm: "SHA1"
     },
 ])
 
-function totp(issuer: string, label: string, secretKey: string, digits: number, period: number, algorithm: string) {
-    const config = new OTPAuth.TOTP({
-        issuer: issuer,
-        label: label,
-        secret: secretKey,
-        digits: digits,
-        period: period,
-        algorithm: algorithm
+function otpObject(entry) {
+    return new OTPAuth.TOTP({
+        issuer: entry.issuer,
+        label: entry.label,
+        secret: entry.secretKey,
+        digits: entry.digits,
+        period: entry.period,
+        algorithm: entry.algorithm
     })
+}
+
+function totpCode(entry) {
+    const config = otpObject(entry);
+    return config.generate();
+}
+
+function timeLeft(entry) {
+    const config = otpObject(entry);
+    return config.period - (Math.floor(Date.now() / 1000) % config.period);
 }
 </script>
 
 <template>
     <main>
-        <TOTPEntry v-for="entry in entries" :secret-key="entry.secretKey" :digits="entry.digits" :period="entry.period" :algorithm="entry.algorithm" :key="entry.id">{{  }}</TOTPEntry>
+        <TOTPEntry v-for="entry in entries" :secret-key="entry.secretKey" :digits="entry.digits" :period="entry.period" :algorithm="entry.algorithm" :otpObject="otpObject(entry)" :key="entry.id">
+            <p>code: {{ totpCode(entry) }}</p>
+            <p>time left: {{ timeLeft(entry) }}</p>
+        </TOTPEntry>
     </main>
 </template>
