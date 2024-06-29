9347cf1cf8c2c21f0f307531cfc384aaf1ca4177
Isaiah <64103005+RuralAnemone@users.noreply.github.com>
2024-06-29 16:55:38 -0500

    add test path

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,10 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from "vue-router";
-import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
     <header>
-        <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-        <div class="wrapper">
-            <HelloWorld msg="You did it! :D" />
-
-            <nav>
-                <RouterLink to="/">Home</RouterLink>
-                <RouterLink to="/about">About</RouterLink>
-            </nav>
-        </div>
+        <h1>TOTP Manager</h1>
     </header>
 
     <RouterView />
