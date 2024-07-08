bcf746004506a4c9716956896f511be410e0db15
Isaiah <64103005+RuralAnemone@users.noreply.github.com>
2024-07-08 22:06:10 +0000

    basic input functionality almost

--- src/components/TOTPInput.vue
+++ src/components/TOTPInput.vue
@@ -1,5 +1,24 @@
+<script setup>
+import { computed, ref } from 'vue';
+// let disabled =
+let totpCode = ref('');
+let disabled = computed(() => {
+    return totpCode.value.length <= 0;
+});
+
+// TODO: get stuff from localStorage
+
+
+
+function handleNewEntry() {
+    // emit an object back to parent
+}
+</script>
+
 <template>
-    <div>
-        <p>arr, input yer data here mateys</p>
+    <div class="totp-input">
+        <p>code (b32 string)</p>
+        <input ref="codeRef" v-model.trim="totpCode" required placeholder="JBSWY3DPEHPK3PXP">
+        <button :disabled @click.prevent="handleNewEntry">{{ disabled ? "enter a valid code" : "submit a new entry!" }}</button>
     </div>
 </template>
